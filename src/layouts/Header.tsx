c3d0175ec650b634fce8cccaf42fe800774cf939
francisBae <qowhdgn@gmail.com>
2024-12-01 00:00:55 +0900

    [ADD] 스타일 수정 및 이미지 교체

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Drawer } from '@mui/material';
+import { AppBar, Toolbar, IconButton, Drawer } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import logo from '../assets/images/main/ap1logo.jpg';
@@ -8,33 +8,81 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 const StyledToolbar = styled(Toolbar)`
 	display: flex;
 	justify-content: space-between;
-	background-color: white;
-	color: black;
+	background-color: white; /* GNB 배경색 흰색 */
+`;
+
+const LogoContainer = styled.div`
+	height: 60px; /* 로고 높이 설정 */
+	background-color: white; /* 로고 배경색 흰색 */
+	display: flex;
+	align-items: center; /* 중앙 정렬 */
+	justify-content: center; /* 중앙 정렬 */
 `;
 
 const Logo = styled.img`
-	height: 50px;
-	margin: 0 auto;
-	display: block;
+	height: 40px; /* 로고 크기 조정 */
 `;
 
 const MenuButton = styled(IconButton)`
 	position: absolute;
 	left: 20px;
-	top: 10px;
+	top: 5px; /* 햄버거 버튼 살짝 위로 위치 조정 */
 `;
 
 const DrawerContainer = styled.div`
 	width: 250px; /* Drawer 너비 설정 */
-	background-color: white; /* Drawer 배경색 흰색 */
+	background-color: #333; /* 어두운 배경색 */
 	height: 100%;
-	padding: 20px;
-	z-index: 1300; /* 높여서 다른 요소 위에 표시 */
-	position: relative; /* Drawer 내부의 요소들에 대한 z-index 처리를 위해 relative 설정 */
+	display: flex;
+	flex-direction: column; /* 세로로 배치 */
+`;
+
+const LogoArea = styled.div`
+	background-color: white; /* 흰색 배경 */
+	padding: 15px 0; /* 상하 여백 추가 */
+	text-align: center; /* 중앙 정렬 */
+	position: relative; /* 버튼을 절대 위치시키기 위한 설정 */
+`;
+
+const MenuArea = styled.div`
+	background-color: #444; /* 회색 배경 */
+	flex-grow: 1; /* 남은 공간을 차지하도록 설정 */
+	display: flex;
+	flex-direction: column; /* 세로로 배치 */
+	align-items: flex-start; /* 왼쪽 정렬 */
+	padding: 10px; /* 패딩 추가 */
+`;
+
+const MenuItem = styled(Link)`
+	color: white; /* 메뉴 버튼 글자 색상 흰색 */
+	width: 100%; /* 전체 너비 */
+	text-align: left; /* 왼쪽 정렬 */
+	margin: 5px 0; /* 위아래 여백 추가 */
+	padding: 10px; /* 패딩 추가 */
+	font-family: '가톨릭체', Arial, sans-serif; /* 폰트 적용 */
+	text-decoration: none; /* 기본 링크 스타일 제거 */
+
+	&:hover {
+		background-color: #555; /* Hover 시 배경색 변경 */
+	}
+`;
+
+const Divider = styled.div`
+	width: 100%; /* 전체 너비 */
+	height: 1px;
+	background-color: white; /* 구분선 색상 흰색 */
+	margin: 0; /* 여백 제거 */
+`;
+
+const CloseButton = styled(IconButton)`
+	position: absolute; /* 절대 위치 */
+	top: -10px; /* 위쪽 여백 */
+	left: 60px; /* 오른쪽 여백 */
+	color: white; /* X 버튼 색상 흰색 */
 `;
 
 const Header: React.FC = () => {
-	const [drawerOpen, setDrawerOpen] = useState(false);
+	const [drawerOpen, setDrawerOpen] = useState(false); // Drawer 상태 관리
 
 	const toggleDrawer = (open: boolean) => () => {
 		setDrawerOpen(open);
@@ -46,28 +94,29 @@ const Header: React.FC = () => {
 				<MenuButton onClick={toggleDrawer(true)}>
 					<FaBars />
 				</MenuButton>
-				<Logo src={logo} alt='압구정1동 청년부 로고' />
+				<LogoContainer>
+					<Logo src={logo} alt='압구정1동 청년부 로고' />
+				</LogoContainer>
 				<Drawer anchor='left' open={drawerOpen} onClose={toggleDrawer(false)}>
 					<DrawerContainer>
-						<IconButton onClick={toggleDrawer(false)}>
-							<FaTimes />
-						</IconButton>
-						<Typography variant='h6'>메뉴</Typography>
-						<Link to='/summary' style={{ textDecoration: 'none', color: 'black' }}>
-							<Button fullWidth>본당소개</Button>
-						</Link>
-						<Link to='/organization' style={{ textDecoration: 'none', color: 'black' }}>
-							<Button fullWidth>단체나눔공간</Button>
-						</Link>
-						<Link to='/precedent' style={{ textDecoration: 'none', color: 'black' }}>
-							<Button fullWidth>정보나눔</Button>
-						</Link>
-						<Link to='/gallery' style={{ textDecoration: 'none', color: 'black' }}>
-							<Button fullWidth>사진갤러리</Button>
-						</Link>
-						<Link to='/location' style={{ textDecoration: 'none', color: 'black' }}>
-							<Button fullWidth>찾아오시는길</Button>
-						</Link>
+						<LogoArea>
+							<Logo src={logo} alt='압구정1동 청년부 로고' />
+							<CloseButton onClick={toggleDrawer(false)}>
+								<FaTimes />
+							</CloseButton>
+						</LogoArea>
+						<MenuArea>
+							<MenuItem to='/summary'>본당소개</MenuItem>
+							<Divider />
+							<MenuItem to='/organization'>단체나눔공간</MenuItem>
+							<Divider />
+							<MenuItem to='/precedent'>정보나눔</MenuItem>
+							<Divider />
+							<MenuItem to='/gallery'>사진갤러리</MenuItem>
+							<Divider />
+							<MenuItem to='/location'>찾아오시는길</MenuItem>
+							<Divider />
+						</MenuArea>
 					</DrawerContainer>
 				</Drawer>
 			</StyledToolbar>
