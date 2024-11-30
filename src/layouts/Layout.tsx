3177b6f324a7c6f6fcdb671aa33f6bd7be0a317e
francisBae <qowhdgn@gmail.com>
2024-12-01 02:00:26 +0900

    [ADD] 이미지 해상도

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,16 +2,25 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import styled from 'styled-components';
 
+const Container = styled.div`
+	max-width: 600px;
+	margin: 0 auto;
+	box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+	background-color: #fff;
+`;
 const Layout: React.FC = () => {
 	return (
-		<div>
+		<Container>
+			{/* <div> */}
 			<Header />
 			{/* <main style={{ padding: '20px' }}> */}
 			<Outlet /> {/* 중첩된 라우트를 여기에 렌더링 */}
 			{/* </main> */}
 			<Footer />
-		</div>
+			{/* </div> */}
+		</Container>
 	);
 };
 
