2a1745e8b8151a8aa9a12d155cfe2e22e243c8f4
francisBae <qowhdgn@gmail.com>
2024-11-30 22:25:55 +0900

    [ADD] build 옵션 정의 및 prettier, lint 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; //file path resolve module
+import { defineConfig } from 'vite';
+// import { wrapperEnv } from './src/utils/getEnv';
 
-// https://vite.dev/config/
+//defineConfig used to define Vite configuration object
 export default defineConfig({
-  plugins: [react()],
-})
+	resolve: {
+		alias: [
+			{ find: '@components', replacement: '/src/components' },
+			{ find: '@', replacement: '/src' }
+		]
+	},
+	plugins: [react()],
+
+	build: {
+		outDir: 'dist',
+		minify: 'esbuild',
+		rollupOptions: {
+			// entry points
+			input: {
+				main: resolve(__dirname, './index.html')
+			},
+			output: {
+				// Static resource classification and packaging
+				chunkFileNames: 'assets/js/[name].[hash:8].js',
+				entryFileNames: 'assets/js/[name].[hash:8].js',
+				assetFileNames: 'assets/[ext]/[name].[hash:8].[ext]'
+			}
+		},
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	}
+});
